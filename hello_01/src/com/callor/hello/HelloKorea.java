23c7a23b01f3e3be740ffc7edf746ee11179a203
kimgukju <kimgukju@kimgukju.com>
2023-04-13 10:17:27 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+		System.out.println(30.0 / 40.0);
+	}
+
+}
