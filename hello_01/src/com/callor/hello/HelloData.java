58a60fa921ffedcad4e37603753e70616d20d869
kimgukju <kimgukju@kimgukju.com>
2023-04-13 14:18:14 +0900

    imoji

---
+++ hello_01/src/com/callor/hello/HelloData.java
@@ -0,0 +1,38 @@
+package com.callor.hello;
+
+public class HelloData {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+		System.out.println("Hello" + "Korea");
+		System.out.println("Hello       Korea");
+		System.out.println("Hello"     +      "Korea");
+		
+		System.out.println(30 + 40);
+		System.out.println("30" + "40");
+		System.out.println("30 + 40");
+		
+		// 30 + 40 = 70
+		System.out.println("30 + 40 = 70");
+		System.out.println("30 + 40 = " + 30 + 40);
+		
+		// "30 + 40" + 30을 연산하려고 시도
+		// 이 연산은 문자열 + 숫자의 연산
+		// 이럴때는 숫자 데이터인 30을 문자열 30으로 바꿔 버린다
+		System.out.println("30 + 40 = " + "30" + 40);
+		System.out.println("30 + 40 = 30" + 40);
+		System.out.println("30 + 40 = " + "30" + "40");
+		System.out.println("30 + 40 = " + (30 + 40));
+		System.out.println(30 + 40 + " = 30 + 40");
+		System.out.println("30 x 40 = " + 30 * 40);
+		// 문자열은 + (연결연산자)만 가능하다
+		// System.out.println("30 - 40 = " + 30 - 40);
+		// System.out.println("30" - "40");
+		// System.out.println("30" * "40");
+		// System.out.println("30" / "40");
+		
+		System.out.println("30" + "40");
+		
+	}
+
+}
