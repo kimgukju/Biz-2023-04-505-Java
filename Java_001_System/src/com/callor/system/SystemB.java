da26f764783f3e9bec3e392e58e8728070578bfc
kimgukju <kimgukju@kimgukju.com>
2023-04-14 10:44:46 +0900

    System클래스

---
+++ Java_001_System/src/com/callor/system/SystemB.java
@@ -0,0 +1,15 @@
+package com.callor.system;
+
+public class SystemB {
+	
+	public static void main(String[] args) {
+		// form에 맞춰서 데이터를 출력하는 method 
+		System.out.printf("%d + %d = %d\n",30,40,30+40);
+		System.out.println();
+		System.out.printf("%d x %d = %d\n",99,77,99*70);
+		System.out.println();
+		System.out.print("\n"); 
+		System.out.print("대한민국\n우리나라\nRepublic of Korea");
+	}
+
+}
